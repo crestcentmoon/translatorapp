5c70706791bfc221cebfa860cc7a55d02de353a6
Asry <68435184+crestcentmoon@users.noreply.github.com>
2020-12-22 11:14:13 +0700

    delete print words, adding menu to choose lang

--- uas.py
+++ uas.py
@@ -9,14 +9,42 @@ text = pytesseract.image_to_string(targeted_image)
 
 print('Tulisan pada gambar:\n', text)
 
-words = text.split()
-print('\nJumlah kata pada gambar about_python')
-print('adalah: ', len(words))
-
-score = len(words)*10
-print('Skor yang bisa didapatkan: $', score)
-
 translator = Translator()
-test1 = translator.translate(text, dest='id')
-print('\nTerjemahan:\n', test1.text)
+def trans():
+    print('\n\t-----------------------------')
+    print('\tKode Bahasa Tersedia')
+    print('\t-------------------------------')
+    print('\t1. Indonesia (id)')
+    print('\t2. Perancis (fr)')
+    print('\t3. Jerman (de)')
+    print('\t-------------------------------')
+    print('')
+    
+    pilih = input("Masukkan kode bahasa: ")
+    
+    if pilih == 'id':
+        paragraph = translator.translate(text, dest='id')
+        print('\nTerjemahan:\n', paragraph.text)
+        tanya()
+    elif pilih == 'fr':
+        paragraph = translator.translate(text, dest='fr')
+        print('\nTerjemahan:\n', paragraph.text)
+        tanya()
+    elif pilih == 'de':
+        paragraph = translator.translate(text, dest='de')
+        print('\nTerjemahan:\n', paragraph.text)
+        tanya()
+    else:
+        print('Maaf, bahasa yang anda pilih belum tersedia')
+        
+def tanya():
+    tanya = input('\n\tKembali ke menu utama? (y/t)')
+    if tanya == 'y':
+        trans()
+    elif tanya == 't':
+        exit
+    else:
+        print('\n\tWrong answer')
+        
+trans()
 
